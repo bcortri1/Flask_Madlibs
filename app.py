49bc5878947e438b029b8327286c8492490bfb8b
Blake C <bcortri1@kent.edu>
2022-11-08 00:40:29 -0500

    templating adjusted

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from flask import Flask, request, render_template
-from stories import story
+from flask_debugtoolbar import DebugToolbarExtension
+import stories
 
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "1234"
+
+debug = DebugToolbarExtension(app)
+
 
 @app.route("/")
 def index():
     title = "Madlibs"
-    prompts = story.prompts
-    
-    return render_template("form.html",title, prompts)
+    prompts = stories.story.prompts
+
+    return render_template("form.html", title=title, prompts=prompts)
+
 
 @app.route("/story")
 def story():
     title = "Story"
-    text = story.generate(request.args)
-    return render_template("story.html",title, text)
-
+    text = stories.story.generate(request.args)
+    return render_template("story.html", title=title, text=text)
